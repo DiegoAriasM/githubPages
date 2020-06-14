c55df5be5af37df4dcbd2f38c71171ca522a1d25
Diego Arias <jack12diego@gmail.com>
2020-06-13 21:49:07 -0500

    cambios

---
+++ AJAX/app.js
@@ -0,0 +1,19 @@
+document.getElementById('cargar').addEventListener('click', cargarDatos);
+
+function cargarDatos() {
+    //Crear el objeto xmlhttprequest
+    const xhr = new XMLHttpRequest();
+
+    // Abrir una conexión
+    xhr.open('GET', 'datos.txt', true);
+
+    xhr.onreadystatechange = function(){
+
+        console.log(`Estado ${this.readyState}`);
+        if(this.readyState === 4 && this.status === 200){
+            //console.log(this.responseText);
+        }
+    }
+    //Enviar el request
+    xhr.send();
+}
